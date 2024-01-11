b5025bc3310ffeb56a9459dd7d2643715eb02478
William <byweimar@gmail.com>
2024-01-10 19:53:34 -0500

    solution

--- server/app.js
+++ server/app.js
@@ -5,5 +5,29 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Sorry, the requested resource couldn\'t be found.');
+  error.statusCode = 404;
+  next(error);
+});
+
+
+app.use((err, req, res, next) => {
+
+  console.error(err);
+
+
+  const statusCode = err.statusCode || 500;
+
+
+  res.status(statusCode);
+
+
+  res.json({
+    message: err.message,
+    statusCode: statusCode,
+  });
+});
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
